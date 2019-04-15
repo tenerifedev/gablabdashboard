4f9ecd3255334fc5af67a160a958964d640f1c62
Santiago Porras Rodríguez <santypr@gmail.com>
2019-04-15 02:29:49 +0100

    Added apexcharts

---
+++ GAB.LabDashboard.Web/gulpfile.js
@@ -0,0 +1,60 @@
+﻿/// <binding Clean='clean'/>
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    rename = require('gulp-rename'),
+    tfs = require("gulp-tfs-checkout");
+
+var webroot = "./wwwroot/";
+
+var paths = {
+    js: webroot + "js/**/*.js",
+    minJs: webroot + "js/**/*.min.js",
+    css: webroot + "css/**/*.css",
+    minCss: webroot + "css/**/*.min.css",
+    concatJsDest: webroot + "js/site.min.js",
+    concatCssDest: webroot + "css/site.min.css",
+    scss: webroot + "css/**/*.scss"
+};
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+//gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task('sass-compiler', function () {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.styles));
+});
+
+gulp.task('sass-watcher', function () {
+    gulp.watch(paths.scss, ['sass-compiler']);
+});
+
+//gulp.task("min", ["min:js", "min:css"]);
+
